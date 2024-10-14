dfc67fb66c14d6e1599425df7997df4b70d98be4
Preveen369 <spreveen123@gmail.com>
2024-10-14 22:09:14 +0530

    Added Literals and Logical operations java files

---
+++ Literals.java
@@ -0,0 +1,26 @@
+class Literals {
+    public static void main(String[] args) {
+        // Literals in java
+
+        /*int a = 10;
+        System.out.println(a);
+
+        int bin = 0B1100;
+        System.out.println(bin);
+
+        int hexnum = 0x6E;
+        System.out.println(hexnum);
+
+        int octalnum = 056;
+        System.out.println(octalnum);
+
+        int large_num = 7_98_00_000;
+        System.out.println(large_num);
+
+        double double_num = 237.34_34E6f;
+        System.out.println(double_num);
+
+        double exp_num = 9.56E3;
+        System.out.println(exp_num);*/
+    }
+}
---
+++ LogicalOp.java
@@ -0,0 +1,14 @@
+class LogicalOp {
+    public static void main(String[] args) {
+        // Logical Operators
+        // && -> AND, || -> OR, ! - NOT
+        
+        int x = 4;
+        int y = 9;
+        int a = 3;
+        int b = 6;
+
+        boolean result = x < y || a < b;
+        System.out.println(!result);
+    }
+}
