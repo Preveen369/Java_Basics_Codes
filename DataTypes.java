faefb22dd3eaf35b975aa3fcc225b5fbfba4e9c4
Preveen369 <spreveen123@gmail.com>
2024-10-14 22:12:03 +0530

    Included varaiables and datatypes java files

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+class DataTypes {
+    public static void main(String[] args) {
+        // Syntax: datatype variable = value;
+        
+        /*int num1 = 56;
+        long num2 = 56234l;
+        short num3 = 4532;
+        byte num4 = 127;
+
+        double num5 = 12.7833;
+        float num6 = 3.45f;
+
+        char letter = 'c';
+        boolean condition = false;*/
+
+        //System.out.println(num1);
+        //System.out.println(num2);
+        //System.out.println(num3);
+        //System.out.println(num3);
+        //System.out.println(num4);
+
+        //System.out.println(num5);
+        //System.out.println(num6);
+
+        //System.out.println(letter);
+        //System.out.println(condition);
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,13 @@
+class Variables {
+    public static void main(String[] args) {
+        // Variables in Java
+        // Syntax: datatype variable = value;
+        
+        int num1 = 4;
+        int num2 = 9;
+        int product = num1*num2;
+        System.out.println(product);
+        System.out.println(2+4);
+
+    }
+}
